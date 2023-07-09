3f67cec6aeb2c69661fa3186a9734caed3d33e48
Subeen Yoo <subeenyoo1002@gmail.com>
2023-07-09 21:38:10 +0900

    Add project files.

---
+++ Source/MyOnlineTPS/MyOnlineTPSGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MyOnlineTPSGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MYONLINETPS_API AMyOnlineTPSGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
